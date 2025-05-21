370c5fdbb7978676483c74569ee59f39d0f60a5e
alvaro.cia <alvarociamina@gmail.com>
2025-05-21 12:57:32 +0200

    fix(namespace): add imports

---
+++ R/utils.R
@@ -0,0 +1,11 @@
+#' @name crm2s-imports
+#' @title Import functions from base packages
+#'
+#' @description This block imports commonly used base R functions
+#'   into the package namespace.
+#'
+#' @importFrom grDevices dev.off pdf
+#' @importFrom graphics hist legend
+#' @importFrom stats IQR binomial coef glm median optim quantile rbinom uniroot var
+#' @importFrom utils head tail
+NULL
